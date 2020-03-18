f7bd4769dced910715355d426ad8ff4d25998e3d
Max Gorankov <maksim.gorankov@gmail.com>
2020-03-18 12:22:50 +0500

    add decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hey yo";
+            Console.WriteLine(new ToUpperText(new ReplaceSpacesWithLog()).GetFormattedText(text));
+
+        }
+    }
+
+    public abstract class TextFormator
+    {
+        private TextFormator _textFormator;
+
+        protected TextFormator(TextFormator textFormator = null)
+        {
+            _textFormator = textFormator;
+        }
+
+        public virtual string GetFormattedText(string text)
+        {
+            if (_textFormator != null)
+            {
+                text = _textFormator.GetFormattedText(text);
+            }
+
+            return text;
+        }
+    }
+
+    class ReplaceSpacesWithLog : TextFormator
+    {
+        public ReplaceSpacesWithLog(TextFormator textFormator = null) : base(textFormator) { }
+
+        public override string GetFormattedText(string text)
+        {
+            Console.WriteLine($"All ' ' will replace to '+'");
+            return base.GetFormattedText(text)?.Replace(' ', '+');
+        }
+    }
+
+    public class ToUpperText : TextFormator
+    {
+        public ToUpperText(TextFormator textFormator = null) : base(textFormator) { }
+
+        public override string GetFormattedText(string text)
+        {
+            return base.GetFormattedText(text).ToUpper();
+        }
+    }
+}
