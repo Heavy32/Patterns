b5d53887c3a48413b4456b5acdf3f7e3d936b7d9
Max Gorankov <maksim.gorankov@gmail.com>
2020-04-05 21:52:45 +0500

    Add singleton

--- Singleton/AccountCounter.cs
+++ Singleton/AccountCounter.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Singleton
 {
-    public sealed class AccountCounter
+    public sealed class LoggerSingleton
     {
-        private static readonly Lazy<AccountCounter> lazy = new Lazy<AccountCounter>(() => new AccountCounter());
+        private static readonly Lazy<LoggerSingleton> instance = new Lazy<LoggerSingleton>(() => new LoggerSingleton());
+        private int logID = 1;
+        private StreamWriter logger;
 
-        public static AccountCounter Instance { get { return lazy.Value; } }
-        public int count = 0;
+        private LoggerSingleton()
+        {
+            logger = new StreamWriter("logger.txt");
+        }
 
-        private AccountCounter() { }
+        public void WriteInLog(string logMessage)
+        {
+            DateTime now = DateTime.Now;
+            logger.Write("Log ID: " + logID + " Date:"  + now + " " + logMessage);
+        }
+
+        public void FinishLogger()
+        {
+            logger.Close();
+        }
+
+        public static LoggerSingleton Instance { get { return instance.Value; } }
     }
 }
