48a3f4be85df479859422f2690c4bc1df83a3da7
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-02 11:52:34 +0500

    Revert "Delete ICar.cs"
    This reverts commit 80b693eecbd8e2052bc02576a9684177c1f54283.

---
+++ Decorator/CarDecorator/ICar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Decorator
+{
+    public interface ICar
+    {
+        public float Cost { get; set; }
+
+        public List<string> Extra { get; set; }
+    }
+}
