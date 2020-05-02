a5207eb57015307c2d77d417567899bca1b90f4e
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-02 11:52:38 +0500

    Revert "Delete FurSitsDecorator.cs"
    This reverts commit ee52eb6dc2f1a1953871e2d491fedc2f4c5ed5c0.

---
+++ Decorator/CarDecorator/FurSitsDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Decorator
+{
+    public class FurSitsDecorator : ICar
+    {
+        private readonly ICar decoratedCar;
+
+        public FurSitsDecorator(ICar decoratedCar)
+        {
+            this.decoratedCar = decoratedCar;
+            Cost = decoratedCar.Cost + 1000;
+            decoratedCar.Extra.ForEach(x => Extra.Add(x));
+            Extra.Add("+ Fur sits ");
+        }
+
+        public float Cost { get; set; }
+        public List<string> Extra { get; set; } = new List<string>();
+    }
+}
