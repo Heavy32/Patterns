431c6869a147969f2b45093ea4bec42bdde1aeb3
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-02 12:08:09 +0500

    Clean Adapter

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+
+namespace Adapter
+{   //NOT FINISHED YET
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdapterSuperSecretClass a = new AdapterSuperSecretClass();
+            a.Action();
+        }
+    }
+
+    class SuperSecretClass
+    {
+        public void SecretAction()
+        {
+            Console.WriteLine("secret");
+        }
+    }
+
+    interface IShow
+    {
+        public void Action();
+    }
+
+    class AdapterSuperSecretClass : SuperSecretClass, IShow
+    {
+        public void Action()
+        {
+            SecretAction();
+        }
+    }
+}
