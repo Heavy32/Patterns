f7be0abb14cccaa74f8697e75ad992087b4199cb
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-02 12:09:55 +0500

    Revert "Revert "Add Decorator""
    This reverts commit 3fadd10ee92d4947e8873a54dbdc4057c487fc5a.

---
+++ Decorator/CarDecorator/BaseCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    class BaseCar : ICar
+    {
+        public BaseCar()
+        {
+            Extra.Add("Basic car conficuration");
+            Cost = 10000;
+        }
+
+        public float Cost { get; set; }
+        public List<string> Extra { get; set; } = new List<string>();
+    }
+}
---
+++ Decorator/TextDecorator/RemoveCapsLockRage.cs
@@ -0,0 +1,12 @@
+﻿namespace Decorator
+{
+    public class RemoveCapsLockRage : TextFilter
+    {
+        public RemoveCapsLockRage(TextFilter textFilter = null) : base(textFilter) { }
+
+        public override string Filter(string text)
+        {
+            return base.Filter(text).ToLower();
+        }
+    }
+}
