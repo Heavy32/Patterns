a375705a35b6f2268a91064dbb74d691ce0f4a61
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-02 11:49:56 +0500

    Revert "Delete InternetAdressRequester.cs"
    This reverts commit f8f68c332263564929ebc1e115894faf74815046.

---
+++ Proxy/InternetAdressRequester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Decorator
+{
+    class InternetAdressRequester : IRequester
+    {
+        private string adress;
+
+        public string Adress { get => adress; set => adress = value; }
+
+        public InternetAdressRequester(string adress)
+        {
+            Adress = adress;
+        }
+
+        public void Request()
+        {
+            Console.WriteLine("Sending request to: " + adress);
+        }
+    }
+}
