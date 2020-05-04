0a7ac3a112b205f92bf96c9e5913da5dada25be9
Max <56233430+Heavy32@users.noreply.github.com>
2020-05-04 15:39:24 +0500

    Update AccountCounter.cs

--- Singleton/AccountCounter.cs
+++ Singleton/AccountCounter.cs
@@ -1,14 +1,9 @@
-﻿using System;
-<<<<<<< HEAD
-=======
-using System.IO;
->>>>>>> parent of 4f4f6a8... Add disposable, add properties, repair
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Singleton
 {
-<<<<<<< HEAD
     public sealed class AccountCounter
     {
         private static readonly Lazy<AccountCounter> lazy = new Lazy<AccountCounter>(() => new AccountCounter());
@@ -17,30 +12,5 @@ namespace Singleton
         public int count = 0;
 
         private AccountCounter() { }
-=======
-    public sealed class LoggerSingleton
-    {
-        private static readonly Lazy<LoggerSingleton> instance = new Lazy<LoggerSingleton>(() => new LoggerSingleton());
-        private int logID = 1;
-        private StreamWriter logger;
-
-        private LoggerSingleton()
-        {
-            logger = new StreamWriter("logger.txt");
-        }
-
-        public void WriteInLog(string logMessage)
-        {
-            DateTime now = DateTime.Now;
-            logger.Write("Log ID: " + logID + " Date:"  + now + " " + logMessage);
-        }
-
-        public void FinishLogger()
-        {
-            logger.Close();
-        }
-
-        public static LoggerSingleton Instance { get { return instance.Value; } }
->>>>>>> parent of 4f4f6a8... Add disposable, add properties, repair
     }
 }
