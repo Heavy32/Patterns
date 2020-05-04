f202c74e1f908d2ffbf5a0ca250d68dc143933f8
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-04 13:51:44 +0500

    Revert "Delete decorator"
    This reverts commit 4a8c9bf8e401b1bfbe7995d4fb8cb8e2a8c061a0.

---
+++ Decorator/TextDecorator/AntiSpam.cs
@@ -0,0 +1,17 @@
+﻿namespace Decorator
+{
+    public class AntiSpam : TextFilter
+    {
+        public AntiSpam(TextFilter textFilter = null) : base(textFilter) { }
+
+        public override string Filter(string text)
+        {
+            text = base.Filter(text);
+
+            if (text.Contains("https://"))
+                text = "Message has been removed because of spam";
+
+            return text;
+        }
+    }
+}
