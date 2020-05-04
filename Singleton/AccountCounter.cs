7b0eb81abe69d4cda673911cd0d9aaada883515b
Max <56233430+Heavy32@users.noreply.github.com>
2020-05-04 14:40:11 +0500

    Update AccountCounter.cs

--- Singleton/AccountCounter.cs
+++ Singleton/AccountCounter.cs
@@ -1,32 +1,16 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Singleton
 {
-    public sealed class LoggerSingleton
+    public sealed class AccountCounter
     {
-        private static readonly Lazy<LoggerSingleton> instance = new Lazy<LoggerSingleton>(() => new LoggerSingleton());
-        private int logID = 1;
-        private StreamWriter logger;
+        private static readonly Lazy<AccountCounter> lazy = new Lazy<AccountCounter>(() => new AccountCounter());
+        
+        public static AccountCounter Instance { get { return lazy.Value; } }
+        public int count = 0;
 
-        private LoggerSingleton()
-        {
-            logger = new StreamWriter("logger.txt");
-        }
-
-        public void WriteInLog(string logMessage)
-        {
-            DateTime now = DateTime.Now;
-            logger.Write("Log ID: " + logID + " Date:"  + now + " " + logMessage);
-        }
-
-        public void FinishLogger()
-        {
-            logger.Close();
-        }
-
-        public static LoggerSingleton Instance { get { return instance.Value; } }
+        private AccountCounter() { }
     }
 }
