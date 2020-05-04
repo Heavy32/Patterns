d9f551c55b79a36f7c423b5d73859ac6853737aa
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-04 15:21:47 +0500

    Clean

---
+++ Singleton/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Account
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Account(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            AccountCounter.Instance.count++;
+        }
+    }
+}
