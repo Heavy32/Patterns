c767d4a2ba1e0e4bb1fb1bbd881b15417b91e667
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-04 14:26:54 +0500

    Revert "Add disposable, add properties, repair"
    This reverts commit 4f4f6a8ce3059f940b174dcc9a800f5309e97e58.

--- Singleton/Account.cs
+++ Singleton/Account.cs
@@ -5,14 +5,14 @@ namespace Singleton
 {
     class Account
     {
-        //public string Name { get; private set; }
-        //public int Age { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
 
-        //public Account(string name, int age)
-        //{
-        //    Name = name;
-        //    Age = age;
-        //    AccountCounter.Instance.count++;
-        //}
+        public Account(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            AccountCounter.Instance.count++;
+        }
     }
 }
