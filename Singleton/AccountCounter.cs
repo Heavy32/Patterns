3bae214d58f45502db73bff645e9bd9bc558bb32
Max <56233430+Heavy32@users.noreply.github.com>
2020-05-04 14:47:15 +0500

    Update AccountCounter.cs

--- Singleton/AccountCounter.cs
+++ Singleton/AccountCounter.cs
@@ -6,7 +6,8 @@ namespace Singleton
 {
     public sealed class AccountCounter
     {
-        private static readonly Lazy<AccountCounter> lazy = new Lazy<AccountCounter>(() => new AccountCounter());        
+        private static readonly Lazy<AccountCounter> lazy = new Lazy<AccountCounter>(() => new AccountCounter());
+        
         public static AccountCounter Instance { get { return lazy.Value; } }
         public int count = 0;
 
