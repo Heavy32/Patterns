19c263ce2fcb7045bbacfa46fb1c1aa0c52519ab
Max <56233430+Heavy32@users.noreply.github.com>
2020-05-04 13:29:51 +0500

    Update Account.cs

--- Singleton/Account.cs
+++ Singleton/Account.cs
@@ -8,11 +8,11 @@ namespace Singleton
         public string Name { get; private set; }
         public int Age { get; private set; }
 
-        //public Account(string name, int age)
-        //{
-        //    Name = name;
-        //    Age = age;
-        //    AccountCounter.Instance.count++;
-        //}
+        public Account(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            AccountCounter.Instance.count++;
+        }
     }
 }
