51e11c20b02b137eff63bf5bb02f4f9f7e0b2a67
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-04 17:54:44 +0500

    Revert "Delete Program.cs"
    This reverts commit 3f960ad9724dc83c0d4f0a2657a65543f02d6794.

---
+++ Decorator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "FUCK YOU https://forums.envato.com/t/check-if-string-is-a-url/75760/2";
+            Console.WriteLine(new ReplaceBadWords(new RemoveCapsLockRage(new AntiSpam())).Filter(text));
+        }
+    }   
+}
