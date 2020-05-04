359df908aad5c583cdd59b39eb10d929825cfe06
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-04 15:39:11 +0500

    Revert "Update AccountCounter.cs"
    This reverts commit 0a7ac3a112b205f92bf96c9e5913da5dada25be9.

--- Singleton/AccountCounter.cs
+++ Singleton/AccountCounter.cs
@@ -1,9 +1,14 @@
-using System;
+﻿using System;
+<<<<<<< HEAD
+=======
+using System.IO;
+>>>>>>> parent of 4f4f6a8... Add disposable, add properties, repair
 using System.Collections.Generic;
 using System.Text;
 
 namespace Singleton
 {
+<<<<<<< HEAD
     public sealed class AccountCounter
     {
         private static readonly Lazy<AccountCounter> lazy = new Lazy<AccountCounter>(() => new AccountCounter());
@@ -12,5 +17,30 @@ namespace Singleton
         public int count = 0;
 
         private AccountCounter() { }
+=======
+    public sealed class LoggerSingleton
+    {
+        private static readonly Lazy<LoggerSingleton> instance = new Lazy<LoggerSingleton>(() => new LoggerSingleton());
+        private int logID = 1;
+        private StreamWriter logger;
+
+        private LoggerSingleton()
+        {
+            logger = new StreamWriter("logger.txt");
+        }
+
+        public void WriteInLog(string logMessage)
+        {
+            DateTime now = DateTime.Now;
+            logger.Write("Log ID: " + logID + " Date:"  + now + " " + logMessage);
+        }
+
+        public void FinishLogger()
+        {
+            logger.Close();
+        }
+
+        public static LoggerSingleton Instance { get { return instance.Value; } }
+>>>>>>> parent of 4f4f6a8... Add disposable, add properties, repair
     }
 }
