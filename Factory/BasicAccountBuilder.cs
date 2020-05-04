11fcf05183dc80d3dc19db8b1880ce0383746351
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-04 17:55:01 +0500

    Revert "Delete files"
    This reverts commit efe88ab4ccfdf8bc345c4b1aefdc8cfffee355aa.

---
+++ Factory/BasicAccountBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace Factory
+{
+    class BasicAccountBuilder : IAccountBuilder
+    {
+        public override Account Create(string name, int age)
+        {
+            return new BasicAccount(name, age);
+        }
+    }
+}
