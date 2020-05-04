ebd174436525638dd3f812e7baaf9e32707730d4
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-04 15:33:19 +0500

    Revert "Delete BasicAccountFactory.cs"
    This reverts commit dd5ed2a47dbbdbef0de6114b8d0d905c97ccd6c2.

---
+++ Factory/AbstractFactory/BasicAccountFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Factory
+{
+    class BasicAccountBuilder : IAccountBuilder
+    {
+        private readonly string name;
+        private readonly int age;
+
+        public BasicAccountBuilder(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Account Create()
+        {
+            return new BasicAccount(name, age);
+        }
+    }
+}
