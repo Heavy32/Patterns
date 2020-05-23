e99528a930cafbc10b78e41f4e6ec80be934f121
Max Gorankov <maksim.gorankov@gmail.com>
2020-05-24 00:47:03 +0500

    Update

--- Singleton/LoggerSingleton.cs
+++ Singleton/LoggerSingleton.cs
@@ -6,23 +6,18 @@ namespace Singleton
     public sealed class LoggerSingleton
     {
         public static readonly Lazy<LoggerSingleton> instance = new Lazy<LoggerSingleton>(() => new LoggerSingleton());
-        private static readonly object syncRoot = new object();
-
+        private readonly object syncRoot = new object();
         private LoggerSingleton() { }
 
         public void WriteInLog(string logMessage)
         {
-            DateTime now = DateTime.Now;
-
-            string writePath = @"C:\Users\eBasher\Documents\GitHub\Patterns\Singleton\Logger.txt";
-
             lock (syncRoot)
             {
-                using StreamWriter logger = new StreamWriter(writePath, true, System.Text.Encoding.Default);
-                    logger.WriteLine("Log ID: " + LogID++ + " Date:" + now + " " + logMessage);
+                using StreamWriter logger = new StreamWriter(@"Logger.txt", true, System.Text.Encoding.Default);
+                    logger.WriteLine("Log ID: " + LogID++ + " Date:" + DateTime.Now + " " + logMessage);
             }           
         }
 
-        public int LogID { get; set; } = 1;
+        public long LogID { get; set; } = 1;
     }
 }
