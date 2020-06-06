571c557d8942e545b3f03e082e1cdbdc8fc01c76
Max Gorankov <maksim.gorankov@gmail.com>
2020-06-06 23:48:16 +0500

    Update

---
+++ Proxy/Proxy.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Proxy
+{
+    class Proxy : WebRequest
+    {
+        private readonly Request request;
+
+        public Proxy()
+        {
+            request = new Request();
+        }
+
+        public WebRequest Create()
+        {
+            return request.RequestWithProxy();
+        }
+    }
+}
---
+++ Proxy/Request.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Proxy
+{
+    public class Request : WebRequest
+    {
+        public HttpWebRequest RequestWithProxy()
+        {
+            WebRequest request = Create("https://docs.microsoft.com");
+            request.Headers["Proxied"] = "true";
+            return (HttpWebRequest)request;
+        }
+    }
+}
