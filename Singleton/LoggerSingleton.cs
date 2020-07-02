897dc364ced65d76745c9b453a5abe83c89fb132
Max <56233430+Heavy32@users.noreply.github.com>
2020-07-02 12:36:37 +0500

    Create LoggerSingleton.cs

---
+++ Singleton/LoggerSingleton.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+namespace Singleton
+{
+    public sealed class LoggerSingleton
+    {
+        public static readonly Lazy<LoggerSingleton> instance = new Lazy<LoggerSingleton>(() => new LoggerSingleton());
+        private readonly object syncRoot = new object();
+        private LoggerSingleton() { }
+
+        public void WriteInLog(string logMessage)
+        {
+            lock (syncRoot)
+            {
+                using StreamWriter logger = new StreamWriter("Logger.txt", true, System.Text.Encoding.Default);
+                    logger.WriteLine("Log ID: " + LogID + " Date:" + DateTime.Now + " " + logMessage);
+            }           
+        }
+
+        public Guid LogID { get => Guid.NewGuid(); }
+    }
+}
